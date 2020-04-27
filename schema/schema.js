53fa4c7de0c36c68d3c07942f7bd097e377502d1
Jerico Valino <jericovalino@gmail.com>
2020-04-27 15:36:25 +0800

    mini

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,57 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLInt,
+    GraphQLID,
+    GraphQLList } = graphql;
+
+let books = [
+    { name: "tom and jerry", genre: "cartoon", id: 1, authorId: 1 },
+    { name: "ninja turtles", genre: "cartoon/action", id: 2, authorId: 2 },
+    { name: "ninjas are awesome", genre: "hehehe", id: 3, authorId: 3 },
+    { name: "clash royale", genre: "cartoon", id: 4, authorId: 2 },
+    { name: "clash of clans", genre: "cartoon/action", id: 5, authorId: 3 },
+    { name: "swabelookbook", genre: "hehehe", id: 6, authorId: 3 }
+]
+
+let authors = [
+    { name: "jerico", age: 21, id: 1 },
+    { name: "ikoy", age: 31, id: 2 },
+    { name: "tikoy", age: 41, id: 3 },
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                console.log(parent)
+                return authors.find(el => el.id == parent.authorId)
+            }
+        }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: { 
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                console.log(parent)
+                return books.filter(el => el.authorId == parent.id)
+            }
+        }
     })
 })
 
@@ -16,12 +60,29 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}},
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // query to database
-                return {
-                    name: "ikoy"
-                }
+                return books.find(el => el.id == args.id)
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return authors.find(el => el.id == args.id)
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books;
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return authors
             }
         }
     }
